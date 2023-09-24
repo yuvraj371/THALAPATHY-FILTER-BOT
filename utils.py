f71247dce5ddec832a3cd540c47697aecf2945fa
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-09-24 14:01:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -668,10 +668,10 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                         reply_markup=InlineKeyboardMarkup(
                             [
                                 [
-                                InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-                                InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
+                                InlineKeyboardButton('🎬Movie Gʀᴏᴜᴘ', url=GRP_LNK),
+                                InlineKeyboardButton('❤️Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ❤️', url=CHNL_LNK)
                             ],[
-                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Madhuri_niranjan")
+                                InlineKeyboardButton("😇Bᴏᴛ Oᴡɴᴇʀ😇", url="t.me/Madhuri_niranjan")
                                 ]
                             ]
                         )
