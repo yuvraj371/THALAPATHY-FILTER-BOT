3815359c801aac3fdabfff9461757be1b47a6442
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-09-27 17:37:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1325,7 +1325,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     InlineKeyboardButton('⛅ Hᴇʟᴘ', callback_data='help'),
                     InlineKeyboardButton('💪 Aʙᴏᴜᴛ 👽', callback_data='about')
                 ],[
-                    InlineKeyboardButton('❓How to Download', url="https://Infinity_XBotz/5"),
+                    InlineKeyboardButton('❓How to Download', url="https://t.me/Infinity_XBotz/5"),
                     InlineKeyboardButton('😇Bot owner😇', url="https://telegram.me/Madhuri_niranjan")
                 ],[
                     InlineKeyboardButton('❤️ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ❤️', url=CHNL_LNK)
