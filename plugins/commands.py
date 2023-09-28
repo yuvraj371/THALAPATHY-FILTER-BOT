16205c2cd26198a86d193441f07b051b6d2db167
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-09-28 11:34:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -190,7 +190,7 @@ async def start(client, message):
                                 InlineKeyboardButton('🎥Movie group', url="https://t.me/+Qn6fthcb7wI0ZTk1")
                             ],[
                                 InlineKeyboardButton("😇Bot owner😇", url="t.me/Madhuri_niranjan"),
-                                InlineKeyboardButtom("❤️Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ❤️", url=CHNL_LNK)
+                                InlineKeyboardButton("❤️Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ❤️", url=CHNL_LNK)
                             ],[   
                                 InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}') #Don't change anything without contacting me @LazyDeveloperr
                             ]
@@ -360,7 +360,7 @@ async def start(client, message):
                                 InlineKeyboardButton('🗃️🎥Movie group', url="https://t.me/+Qn6fthcb7wI0ZTk1")
                             ],[
                                 InlineKeyboardButton("😇Bot owner😇", url="t.me/Madhuri_niranjan"),
-                                InlineKeyboardButtom("❤️Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ❤️", url=CHNL_LNK)
+                                InlineKeyboardButton("❤️Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ❤️", url=CHNL_LNK)
                             ],[   
                                 InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}') #Don't change anything without contacting me @LazyDeveloperr
                             ]
@@ -443,7 +443,7 @@ async def start(client, message):
                                 InlineKeyboardButton('🎥 Movie 🍿 group', url="https://t.me/+Qn6fthcb7wI0ZTk1")
                             ],[
                                 InlineKeyboardButton("😇Bot owner😇", url="t.me/Madhuri_niranjan"),
-                                InlineKeyboardButtom("❤️ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ❤️", url=CHNL_LNK)
+                                InlineKeyboardButton("❤️ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ❤️", url=CHNL_LNK)
                             ],[   
                                 InlineKeyboardButton('🚀 Fast Download / Watch Online🖥️', callback_data=f'generate_stream_link:{file_id}') #Don't change anything without contacting me @LazyDeveloperr
                             ]
