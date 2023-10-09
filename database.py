209b4dff44c40eb1cb1c3e2d95faa642d5224914
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-10-09 08:22:06 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,19 @@
+import asyncio
+import motor.motor_asyncio
+
+async def main():
+    # Establish a MongoDB connection using motor
+    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://your-mongodb-uri")
+    db = client["your-database-name"]
+
+    # Perform your database operations here
+    # ...
+
+    # Close the MongoDB connection
+    client.close()
+
+# Invoke the asynchronous function using asyncio
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+  
