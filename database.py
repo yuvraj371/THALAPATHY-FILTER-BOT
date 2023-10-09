43dce4cb4c0805ece1361e46f32f171c21068f9a
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-10-09 08:23:19 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import motor.motor_asyncio
 
 async def main():
     # Establish a MongoDB connection using motor
-    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://your-mongodb-uri")
+    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://krishna527062:yuvraj178@cluster0.bki9yaw.mongodb.net/?retryWrites=true&w=majority")
     db = client["your-database-name"]
 
     # Perform your database operations here
