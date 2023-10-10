6dc8c7cd81ddf927d0a011ec4c08cbee0bc44e6f
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-10-10 20:40:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -15,14 +15,10 @@ Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
  ‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ2.7.1 [sᴛᴀʙʟᴇ]></b>"""
 
     SOURCE_TXT = """
-<b>✨Hᴇʏ👋, Tʜɪs ɪs not an Oᴘᴇɴ Sᴏᴜʀᴄᴇ Pʀᴏᴊᴇct
-         so if you want this bot repo
-         contact 🔗 to bot owner 👨‍💻\n\n
-✨Tʜɪs Bᴏᴛ ʜᴀs Lᴀᴛᴇsᴛ ᴀɴᴅ Aᴅᴠᴀɴᴄᴇᴅ Fᴇᴀᴛᴜʀᴇs⚡️
-you can add this bot in your group
-and get unlimited movies and web series\n
+<b>✨Hᴇʏ👋, Tʜɪs ɪs not an Oᴘᴇɴ Sᴏᴜʀᴄᴇ Pʀᴏᴊᴇct so if you want this bot repo contact 🔗 to bot owner 👨‍💻\n
+✨Tʜɪs Bᴏᴛ ʜᴀs Lᴀᴛᴇsᴛ ᴀɴᴅ Aᴅᴠᴀɴᴄᴇᴅ Fᴇᴀᴛᴜʀᴇs⚡️you can add this bot in your group and get unlimited movies and web series\n
 ✨special Thanks To our ❤️ Developer 😁\n
-Developer[Owner of this bot]<a href='https://t.me/Madhuri_niranjan'> Madhuri niranjan </a>
+Developer[Owner of this bot]<a href='https://t.me/Madhuri_niranjan'>Madhuri</a>
 \nBot channel <a href='https://t.me/Infinity_XBotz'> Infinity XBotz </a>
 """
 
