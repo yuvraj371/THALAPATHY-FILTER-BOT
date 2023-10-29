074b50c23952bdf055f39a4fa83175c2429aac34
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-10-29 10:31:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,25 +96,25 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '181.214.152.133'))
-    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '181.214.152.133:49152') else APP_NAME + '.herokuapp.com'
+
+BIND_ADDRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '181.214.152.133'))
+FQDN = str(getenv('FQDN', BIND_ADDRESS)) if not ON_HEROKU else APP_NAME + '.herokuapp.com'
+URL = f"https://advance-auto-filter-bot-ql8l.onrender.com/{FQDN}" if ON_HEROKU or NO_PORT else \
+    f"https://advance-auto-filter-bot-ql8l.onrender.com/{FQDN}:{PORT}"
+
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
 MULTI_CLIENT = False
 name = str(environ.get('name', 'LazyPrincess'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-    APP_NAME = environ.get('APP_NAME')
+HAS_SSL = bool(getenv('HAS_SSL', False))
+
+if HAS_SSL:
+    URL = f"https://advance-auto-filter-bot-ql8l.onrender.com/{FQDN}"
 else:
-    ON_HEROKU = False
-    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '181.214.152.133:49152') else APP_NAME + '.herokuapp.com'
-    HAS_SSL = bool(getenv('HAS_SSL', False))
-    if HAS_SSL:
-        URL = "https://{}/".format(FQDN)
-    else:
-        URL = "http://{}/".format(FQDN)
+    URL = f"http://advance-auto-filter-bot-ql8l.onrender.com/{FQDN}"
+
   
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
