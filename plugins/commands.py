d474b9967c703341f8423309d9aa5b7b9f392848
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-11-01 21:50:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,9 +8,10 @@ from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import *
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id, get_bad_files
 from database.users_chats_db import db
-from info import CHANNELS, ADMINS, FIRST_CHANNEL, SECOND_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHNL_LNK, CHNL2_LNK, GRP_LNK, REQST_CHANNEL, SUPPORT_CHAT_ID, SUPPORT_CHAT, MAX_B_TN, VERIFY, SHORTLINK_API, SHORTLINK_URL, TUTORIAL, IS_TUTORIAL, PREMIUM_USER
+from info import CHANNELS, ADMINS, FIRST_CHANNEL, SECOND_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHNL_LNK, GRP_LNK, REQST_CHANNEL, SUPPORT_CHAT_ID, SUPPORT_CHAT, MAX_B_TN, VERIFY, SHORTLINK_API, SHORTLINK_URL, TUTORIAL, IS_TUTORIAL, PREMIUM_USER
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, get_shortlink, get_tutorial
 from database.connections_mdb import active_connection
+# from plugins.pm_filter import ENABLE_SHORTLINK
 import re, asyncio, os, sys
 import json
 import base64
@@ -22,24 +23,24 @@ BATCH_FILES = {}
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
-            InlineKeyboardButton('👻 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ 👻', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-        ], [
-            InlineKeyboardButton('👨‍💻Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url="https://t.me/Infinity_XBotz_support"),
-            InlineKeyboardButton('🎬 Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url='https://t.me/+Qn6fthcb7wI0ZTk1')
-        ], [
-            InlineKeyboardButton('❓How to Download', url="https://t.me/Infinity_XBotz/5"), 
-            InlineKeyboardButton('😇Bot owner😇', url="https://t.me/Madhuri_niranjan")
-        ], [
-            InlineKeyboardButton('❤️ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ❤️', url=CHNL_LNK)
-        ], [
-            InlineKeyboardButton('❤️ Jᴏɪɴ Movie Cʜᴀɴɴᴇʟ ❤️', url=CHNL2_LNK)
-        ]]
+                        InlineKeyboardButton('👻 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ 👻', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                   ], [
+                        InlineKeyboardButton('👨‍💻Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url="https://t.me/Infinity_XBotz_support"),
+                        InlineKeyboardButton('🎬 Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url='https://t.me/+Qn6fthcb7wI0ZTk1')
+                   ], [
+                        InlineKeyboardButton('❓How to Download', url="https://t.me/Infinity_XBotz/5"), 
+                        InlineKeyboardButton('😇Bot owner😇', url="https://t.me/Madhuri_niranjan")
+                   ], [
+                        InlineKeyboardButton('❤️ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ❤️', url=CHNL_LNK)
+                   ], [
+                        InlineKeyboardButton('❤️ Jᴏɪɴ Movie Cʜᴀɴɴᴇʟ ❤️', url=CHNL2_LNK)
+                  ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
-        await asyncio.sleep(2)
+        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
-            total = await client.get_chat_members_count(message.chat.id)
-            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))
+            total=await client.get_chat_members_count(message.chat.id)
+            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
             await db.add_chat(message.chat.id, message.chat.title)
         return 
     if not await db.is_user_exist(message.from_user.id):
@@ -47,23 +48,23 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('👻 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ 👻', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-        ], [
-            InlineKeyboardButton('👨‍💻support group', url="https://telegram.me/Infinity_XBotz_support"),
-            InlineKeyboardButton('🎬 Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url='https://t.me/+Qn6fthcb7wI0ZTk1')
-        ], [
-            InlineKeyboardButton('⛅ Hᴇʟᴘ', callback_data='help'),
-            InlineKeyboardButton('👽 Aʙᴏᴜᴛ', callback_data='about')
-        ], [
-            InlineKeyboardButton('❓How to Download', url="https://t.me/Infinity_XBotz/5"),
-            InlineKeyboardButton('😇Bot owner😇', url="https://telegram.me/Madhuri_niranjan")
-        ], [
-            InlineKeyboardButton('❤️ Join Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ❤️', url=CHNL_LNK)
-        ], [
-            InlineKeyboardButton('❤️ Jᴏɪn Movie Cʜᴀɴɴᴇl ❤️', url=CHNL2_LNK)
-        ]]
+                    InlineKeyboardButton('👻 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ 👻', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+               ], [
+                    InlineKeyboardButton('👨‍💻support group', url="https://telegram.me/Infinity_XBotz_support"),
+                    InlineKeyboardButton('🎬 Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url='https://t.me/+Qn6fthcb7wI0ZTk1')
+               ], [
+                    InlineKeyboardButton('⛅ Hᴇʟᴘ', callback_data='help'),
+                    InlineKeyboardButton('👽 Aʙᴏᴜᴛ', callback_data='about')
+               ], [
+                    InlineKeyboardButton('❓How to Download', url="https://t.me/Infinity_XBotz/5"),
+                    InlineKeyboardButton('😇Bot owner😇', url="https://telegram.me/Madhuri_niranjan")
+               ], [
+                    InlineKeyboardButton('❤️ Join Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ❤️', url=CHNL_LNK)
+               ], [
+                    InlineKeyboardButton('❤️ Jᴏɪn Movie Cʜᴀɴɴᴇl ❤️', url=CHNL2_LNK)
+                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m = await message.reply_sticker("CAACAgUAAxkBAAEBvlVk7YKnYxIHVnKW2PUwoibIR2ygGAACBAADwSQxMYnlHW4Ls8gQHgQ") 
+        m=await message.reply_sticker("CAACAgUAAxkBAAEBvlVk7YKnYxIHVnKW2PUwoibIR2ygGAACBAADwSQxMYnlHW4Ls8gQHgQ") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
@@ -87,21 +88,21 @@ try:
             return
 
         btn = [
-            [
+               [
                 InlineKeyboardButton("❤️ Updates channel ❤️", url=invite_link1.invite_link),
                 InlineKeyboardButton("❤️ Movie channel ❤️", url=invite_link2.invite_link),
-            ],
-            [
+               ],
+               [
                 InlineKeyboardButton('🤔 Why I Am Joining? 🤔', url='https://telegram.me/Infinity_XBotz')
-            ]
-        ]
+               ]
+              ]
 
         if message.command[1] != "subscribe":
             try:
                 kk, file_id = message.command[1].split("_", 1)
-                btn.append([InlineKeyboardButton("↻ Try Again", callback_data=f"checksub#{kk}#{file_id}")])
+                btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", callback_data=f"checksub#{kk}#{file_id}")])
             except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton("↻ Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+                btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_photo(
             chat_id=message.from_user.id,
             photo="https://telegra.ph/file/20b4aaaddb8aba646e53c.jpg",
@@ -114,7 +115,7 @@ try:
                     "उसके बाद आपको आपकी मूवी फ़ाइलें मिल जाएंगी।।...**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
-        )
+            )
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
@@ -134,7 +135,7 @@ try:
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
-        )                                  
+        )
         return
     data = message.command[1]
     try:
